6522d215c3e271fe15e09182b77cb58a5761b4a3
Adam Fehr <adam.fehr.17@cnu.edu>
2020-02-13 13:29:43 -0800

    Still working on it but i still dont have permission to run the project 2.cpp

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -26,8 +26,10 @@ struct entry{
 	int number_occurences;
 };
 
+int arrLength = 1;
+
 //TODO add a global array of entry structs (global to this file)
-entry fileEntries [1];
+entry* fileEntries  = new entry[arrLength];
 
 //TODO add variable to keep track of next available slot in array
 int nextAvailable = 0;
@@ -38,7 +40,7 @@ void clearArray(){
 }
 
 int getArraySize(){
-	return (sizeof(fileEntries)/sizeof(*fileEntries));
+	return arrLength;
 }
 
 std::string getArrayWordAt(int i){
@@ -68,19 +70,57 @@ bool openFile(std::fstream& myfile, const string& myFileName,
 
 	myfile.open(myFileName.c_str(), ios::in);
 
-	if(myfile.is_open()){
-		return true;
-	}
-	else {
-		return false;
+	return myfile.is_open();
+}
+
+void extractTokensFromLine(std::string &myString) {
+	stringstream ss(myString);
+	string tempToken;
+
+	while(getline(ss, tempToken, constants::CHAR_TO_SEARCH_FOR)){
+		processToken(tempToken);
 	}
 }
 
 void processLine(std::string &myString){
 
+	stringstream ss(myString);
+		string tempToken;
+
+		while(getline(ss, tempToken, constants::CHAR_TO_SEARCH_FOR)){
+			processToken(tempToken);
+		}
 }
 
 void processToken(std::string &token){
+	if(nextAvailable == arrLength){
+
+		entry* bigger_arr = new entry[arrLength ];
+
+		for(int i = 0; i < arrLength; i++){
+			bigger_arr[i] = fileEntries[i];
+		}
+
+		arrLength++;
+		delete [] fileEntries;
+		fileEntries = bigger_arr;
+	}
+
+	bool added = false;
+
+	for(int x = 0; x < arrLength; x++){
+		if(token == fileEntries[x].word){
+			fileEntries[x].number_occurences ++;
+			added = true;
+			break;
+
+		}
+	}
+	if(!added){
+		fileEntries[nextAvailable].word = token;
+		fileEntries[nextAvailable].number_occurences = 1;
+		nextAvailable++;
+	}
 
 }
 
@@ -96,6 +136,26 @@ int writeArraytoFile(const std::string &outputfilename){
 
 void sortArray(constants::sortOrder so){
 
+	string tempWord;
+	int tempOccurences;
+
+	for (int i = 0; i < arrLength; ++i)
+	{
+	    for (int j = i + 1; j < arrLength; ++j)
+	    {
+	    	if (fileEntries[i].word > fileEntries[j].word)
+	    	            {
+	    	                tempWord =  fileEntries[i].word;
+	    	                tempOccurences = fileEntries[i].number_occurences;
+
+	    	                fileEntries[i].word = fileEntries[j].word;
+	    	                fileEntries[i].number_occurences = fileEntries[j].number_occurences;
+
+	    	                fileEntries[j].word = tempWord;
+	    	                fileEntries[j].number_occurences = tempOccurences;
+	    	            }
+	    }
+	}
 }
 
 
