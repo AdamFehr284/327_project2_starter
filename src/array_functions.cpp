8d5232a9727cfeb89b9c2f7063d061153efc38af
Adam Fehr <adam.fehr.17@cnu.edu>
2020-02-15 15:57:19 -0800

    meh

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -1,7 +1,7 @@
 /*
  * functionstocomplete.cpp
  *
- *  Created on: Sep 10, 2017
+ *  Created on: feb 13, 2020
  *      Author: Adam Fehr
  */
 
@@ -19,9 +19,8 @@
 //============================================================================
 using namespace std;
 
-
 //TODO define a structure to track words and number of times they occur
-struct entry{
+struct entry {
 	string word;
 	int number_occurences;
 };
@@ -29,44 +28,46 @@ struct entry{
 int arrLength = 1;
 
 //TODO add a global array of entry structs (global to this file)
-entry* fileEntries  = new entry[arrLength];
+entry *fileEntries = new entry[arrLength];
 
 //TODO add variable to keep track of next available slot in array
 int nextAvailable = 0;
 
 //TODO define all functions in header file
-void clearArray(){
-
+void clearArray() {
+	arrLength = 1;
+	nextAvailable = 0;
+	entry *fileEntries = new entry[arrLength];
 }
 
-int getArraySize(){
+int getArraySize() {
 	return arrLength;
 }
 
-std::string getArrayWordAt(int i){
+std::string getArrayWordAt(int i) {
 	return fileEntries[i].word;
 }
 
-int getArrayWord_NumbOccur_At(int i){
+int getArrayWord_NumbOccur_At(int i) {
 	return fileEntries[i].number_occurences;
 }
 
-bool processFile(std::fstream &myfstream){
+bool processFile(std::fstream &myfstream) {
 
 	string str;
 
-	if(!myfstream.is_open()){
+	if (!myfstream.is_open()) {
 		return false;
 	}
-	while(getline(myfstream, str)){
+	while (getline(myfstream, str)) {
 		processLine(str);
 	}
 
 	return true;
 }
 
-bool openFile(std::fstream& myfile, const string& myFileName,
-		std::ios_base::openmode mode){
+bool openFile(std::fstream &myfile, const string &myFileName,
+		std::ios_base::openmode mode) {
 
 	myfile.open(myFileName.c_str(), ios::in);
 
@@ -77,46 +78,46 @@ void extractTokensFromLine(std::string &myString) {
 	stringstream ss(myString);
 	string tempToken;
 
-	while(getline(ss, tempToken, constants::CHAR_TO_SEARCH_FOR)){
+	while (getline(ss, tempToken, constants::CHAR_TO_SEARCH_FOR)) {
 		processToken(tempToken);
 	}
 }
 
-void processLine(std::string &myString){
+void processLine(std::string &myString) {
 
 	stringstream ss(myString);
-		string tempToken;
+	string tempToken;
 
-		while(getline(ss, tempToken, constants::CHAR_TO_SEARCH_FOR)){
-			processToken(tempToken);
-		}
+	while (getline(ss, tempToken, constants::CHAR_TO_SEARCH_FOR)) {
+		processToken(tempToken);
+	}
 }
 
-void processToken(std::string &token){
-	if(nextAvailable == arrLength){
+void processToken(std::string &token) {
+	if (nextAvailable == arrLength) {
 
-		entry* bigger_arr = new entry[arrLength ];
+		entry *bigger_arr = new entry[arrLength];
 
-		for(int i = 0; i < arrLength; i++){
+		for (int i = 0; i < arrLength; i++) {
 			bigger_arr[i] = fileEntries[i];
 		}
 
 		arrLength++;
-		delete [] fileEntries;
+		delete[] fileEntries;
 		fileEntries = bigger_arr;
 	}
 
 	bool added = false;
 
-	for(int x = 0; x < arrLength; x++){
-		if(token == fileEntries[x].word){
-			fileEntries[x].number_occurences ++;
+	for (int x = 0; x < arrLength; x++) {
+		if (token == fileEntries[x].word) {
+			fileEntries[x].number_occurences++;
 			added = true;
 			break;
 
 		}
 	}
-	if(!added){
+	if (!added) {
 		fileEntries[nextAvailable].word = token;
 		fileEntries[nextAvailable].number_occurences = 1;
 		nextAvailable++;
@@ -124,44 +125,49 @@ void processToken(std::string &token){
 
 }
 
-
-
-void closeFile(std::fstream& myfile){
+void closeFile(std::fstream &myfile) {
 	myfile.close();
 }
 
-int writeArraytoFile(const std::string &outputfilename){
-	return 1;
+int writeArraytoFile(const std::string &outputfilename) {
+	ofstream myOutputfile(outputfilename);
+	myOutputfile.open(outputfilename.c_str());
+
+	if(!myOutputfile.is_open()){
+		return constants::FAIL_FILE_DID_NOT_OPEN;
+	}
+
+	if(nextAvailable == 0){
+		return constants::FAIL_NO_ARRAY_DATA;
+	}
+
+	myOutputfile << fileEntries ->entry;
+	myOutputfile.close();
+
+	return constants::SUCCESS;
 }
 
-void sortArray(constants::sortOrder so){
+void sortArray(constants::sortOrder so) {
 
 	string tempWord;
 	int tempOccurences;
 
-	for (int i = 0; i < arrLength; ++i)
-	{
-	    for (int j = i + 1; j < arrLength; ++j)
-	    {
-	    	if (fileEntries[i].word > fileEntries[j].word)
-	    	            {
-	    	                tempWord =  fileEntries[i].word;
-	    	                tempOccurences = fileEntries[i].number_occurences;
+	for (int i = 0; i < arrLength; ++i) {
+		for (int j = i + 1; j < arrLength; ++j) {
+			if (fileEntries[i].word > fileEntries[j].word) {
+				tempWord = fileEntries[i].word;
+				tempOccurences = fileEntries[i].number_occurences;
 
-	    	                fileEntries[i].word = fileEntries[j].word;
-	    	                fileEntries[i].number_occurences = fileEntries[j].number_occurences;
+				fileEntries[i].word = fileEntries[j].word;
+				fileEntries[i].number_occurences =
+						fileEntries[j].number_occurences;
 
-	    	                fileEntries[j].word = tempWord;
-	    	                fileEntries[j].number_occurences = tempOccurences;
-	    	            }
-	    }
+				fileEntries[j].word = tempWord;
+				fileEntries[j].number_occurences = tempOccurences;
+			}
+		}
 	}
 }
-
-
-
-
-
 
 //TODO look in utilities.h for useful functions, particularly strip_unwanted_chars!
 
